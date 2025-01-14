66f587ac99e7c6c204e58351379ca2cb3a95799a
code-n-cry <egor.schukiv@gmail.com>
2025-01-15 01:00:22 +0300

    Fixed structure; add bootstrap cdn links; started user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(verbose_name="Ваше имя(до 50 символов)", name="username", max_length=50)
+    email = models.EmailField(verbose_name="Ваша почта", name="email", unique=True)
+    password = models.CharField(verbose_name="Ваш пароль", max_length=100)
+    
