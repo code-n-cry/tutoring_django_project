a01aa70abded59d663905bcec175d73397d68141
Егор Щукин <e.v.schukin@yandex.ru>
2025-01-25 17:58:31 +0300

    Add logging in

--- user/views.py
+++ user/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login
 
 
 class LoginView(FormView):
     template_name = "user/login_form.html"
     form_class = LoginForm
     success_url = reverse_lazy("mem:index")
+
+    def post(self, request, *args, **kwargs):
+        user = authenticate(request, username=request.POST["username"],
+                            password=request.POST["password"])
+        if user:
+            login(request, user)
+        return super().post(request, *args, **kwargs)
 
 
 class SignUpView(CreateView):
